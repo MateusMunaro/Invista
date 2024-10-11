e5d329e1e0f1b8f5cd7eb96b50feb76724aec340
veiga379 <132915361+veiga379@users.noreply.github.com>
2024-10-11 16:04:43 -0300

    primeiro commit

--- Investimentos/Controllers/APIFinanceController.cs
+++ Investimentos/Controllers/APIFinanceController.cs
@@ -22,6 +22,7 @@ namespace Investimentos.Controllers
       return View("Cripto"); // Certifique-se de que a view Cripto.cshtml esteja na pasta Finance
     }
 
+    
     public async Task<ActionResult> Index()
     {
       var symbols = new List<string> { "IBM", "AAPL", "GOOGL" }; // Adicione os símbolos desejados
@@ -35,14 +36,17 @@ namespace Investimentos.Controllers
 
           // Processando "Meta Data" e "Time Series (Daily)"
           var metaData = data["Meta Data"];
-          var timeSeries = data["Time Series (Daily)"];
+          var timeSeries = (JObject)data["Time Series (Daily)"]; // Aqui, converta para JObject
 
           string symbolFromApi = metaData["2. Symbol"].ToString();
-          foreach (var dateEntry in timeSeries)
-          {
-            var date = DateTime.Parse(dateEntry.Path); // Data como chave
-            var stockInfo = (JObject)dateEntry.First; // Valores diários
 
+          // Iterar sobre as propriedades do timeSeries
+          foreach (var dateEntry in timeSeries.Properties())
+          {
+            var date = DateTime.Parse(dateEntry.Name); // A chave (Name) é a data
+            var stockInfo = (JObject)dateEntry.Value; // O valor é um JObject com os valores diários
+
+            // Adicionar os dados à lista
             stockDataList.Add(new WeeklyStockData
             {
               Symbol = symbolFromApi, // Pega o símbolo da Meta Data
@@ -57,17 +61,8 @@ namespace Investimentos.Controllers
         }
         catch (Exception ex)
         {
-          // Verifica se a mensagem de erro indica que o limite foi atingido
-          if (ex.Message.Contains("rate limit"))
-          {
-            ModelState.AddModelError("", "Você atingiu o limite de solicitações da API. Tente novamente mais tarde.");
-            return View("Error"); // Retorne uma view de erro ou uma página apropriada
-          }
-          else
-          {
-            // Outros erros
-            ModelState.AddModelError("", $"Erro ao processar o símbolo {symbol}: {ex.Message}");
-          }
+          // Tratar erros se necessário
+          Console.WriteLine(ex.Message); // Log de erro
         }
       }
 
